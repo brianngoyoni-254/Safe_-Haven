2662eca3428f9e6259c15722744e2ef86d8a1aa3
Brian Ngoyoni <brianngoyoni254@gmail.com>
2026-07-25 21:09:48 +0300

    Test: empty migration to verify Supabase auto-migration action

---
+++ backend/migrations/versions/889865494546_test_supabase_auto_migration.py
@@ -0,0 +1,24 @@
+"""test supabase auto migration
+
+Revision ID: 889865494546
+Revises: daec1263540e
+Create Date: 2026-07-25 21:07:53.469195
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = '889865494546'
+down_revision = 'daec1263540e'
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
